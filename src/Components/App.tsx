237d7f8cd5cbcc3e7e47e28bbedca7b2303bae11
coderB <flack319@gmail.com>
2021-02-23 14:40:55 +0900

    join design

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,12 +3,16 @@ import { HashRouter, Route } from "react-router-dom";
 
 // routes 
 import Logout from "../routes/Logout";
+import Login from "../routes/Login";
+import Signup from "../routes/Signup";
 
 function App() {
   return (
     <div className="container">
       <HashRouter>
         <Route path="/" exact component={Logout} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
       </HashRouter>
     </div>
   );
--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC = () => {
     //  어떠한 이유로 콘솔을 한 번 찍어주지 않으면
     // FontAwesomeIcon의 icon 경로를 읽어들이지 않는다.
     console.log(faTwitter);
-    console.clear();
+    // console.clear();
     return (
         <div className="logo">
             <FontAwesomeIcon icon={["fab", "twitter"]} />
